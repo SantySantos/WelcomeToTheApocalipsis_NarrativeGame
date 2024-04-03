7484ebfc29d130f7b3fedefa0747c3d98653b81f
SantySantos <santysantos533@gmail.com>
2024-04-03 10:45:55 -0400

    Committing and push

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FINAL_PROJECT_GV5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
