48ba6f509a9ed149980fdb9e1408bad152149ad1
Santiago <santysantos533@gmail.com>
2024-04-03 16:38:29 -0400

    Shorter description
    Shorter and easier to read conserving the information. Missing to add Gunsmith

--- Places/Refugee.cs
+++ Places/Refugee.cs
@@ -13,25 +13,11 @@ namespace OOP_PROJECT.Places
         internal override string Description()
         {
             Console.WriteLine();
-            return @"To the left, a modest [store] offers essential supplies and equipment 
-to aid the player on their journey. The storekeeper, a grizzled old survivor, nods in
-greeting, ready to assist the player with their needs.
-
-                
-Straight ahead lies the entrance to the [forest], a once lush and vibrant expanse now shrouded in mystery and danger. 
-The trees whisper ancient secrets, beckoning the player to explore their depths and uncover hidden treasures.
-
-                
-To the right, the entrance to the [dungeon] looms ominously. Dark and foreboding, it is said to hold untold riches 
-and unimaginable horrors. But to reach the dungeon, the player must first brave the perils of the forest, where danger 
-lurks behind every tree. Deep within the dungeon, guarded by a fearsome monster, are the Crystals of Ages. These legendary artifacts are said to hold
-the power to restore life to the world. Only the bravest and most skilled adventurers dare to face the monster and claim the crystals.
-
-
-
-The refuge is a place of respite and preparation
-
-Write [1] to see your actual stats
+            return @"To the left, a humble [store] offers supplies. 
+Straight ahead, the [forest] whispers secrets. 
+To the right, the [dungeon] holds riches and horrors.
+Only the bravest reach the Crystals of Ages. 
+The refuge is a place of respite. Write [1] for stats.
 
 
 
