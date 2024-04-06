759f629189f1355212d344560b8ffd939830e750
Santiago <santysantos533@gmail.com>
2024-04-06 02:16:07 -0400

    Blacksmith (fifth room)
    We added a black smith where our main character can buy their weapons to fight the final boss.

--- Places/GunSmith.cs
+++ Places/GunSmith.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_PROJECT.Main_Character_Description;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,72 @@ namespace OOP_PROJECT.Places
 {
     internal class BlackSmith : Place
     {
+
         internal override string Description()
         {
-            throw new NotImplementedException();
+            return @"Ah, welcome, welcome! What brings you to my forge today?
+In need of a repair, or perhaps something new? Whatever it is, you've come to the right place.
+Come, let's have a look and see what we can do for you
+
+1.Rusted machete - 5 gold
+2.Shockwave Grenade - 50 gold
+3.Plasma Repeater - 500 gold
+4.Electro-Katana - 1500 gold
+5.EPRK - 2 skulls 
+6.Back to Refugee";
+        }
+        public string GoldReturn(int price)
+        {
+            Characters MainCharacter = Main_Character_Description.Switch.MainCharacter;
+
+            if (MainCharacter.gold - price >= 0)
+            {
+                MainCharacter.gold -= price;
+                return "you have " + MainCharacter.gold.ToString() + "left";
+            }
+            else
+            {
+                return "You dont have enough Money";
+            }
+        }
+        public bool HaveSkulls(bool OneSkull, bool TwoSkull)
+        {
+            if(OneSkull == true && TwoSkull == true) 
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
         }
         internal override void MovingAround(string choice2)
         {
-            throw new NotImplementedException();
+            var game = new Game();
+            switch (choice2)
+            {
+                case "1":
+                    GoldReturn(5);
+                    break;
+                case "2":
+                    GoldReturn(50);
+                    break;
+                case "3":
+                    GoldReturn(500);
+                    break;
+                    case "4":
+                    GoldReturn(1500);
+                    break;
+                    case "5":
+                    HaveSkulls(game.Firstskull, game.SecondSkull);
+                    break; 
+                case "6":
+                    Game.Transition<Refugee>();
+                    break;
+                default:
+                    Console.WriteLine("Choose a valid option");
+                    break;
+            }
         }
     }
 }
