71683da8ec00fc77460de591bae23a71a743da95
Santiago <santysantos533@gmail.com>
2024-04-15 23:19:11 -0400

    Improving Betting system
    Improve of a betting system

--- Places/Inventory.cs
+++ Places/Inventory.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using OOP_PROJECT.Main_Character_Description;
 using System.Threading.Channels;
 using System;
+using System.ComponentModel.Design;
 
 namespace FINAL_PROJECT_GV5.Places
 {
@@ -14,22 +15,27 @@ namespace FINAL_PROJECT_GV5.Places
     {
         static double Fruits = 0;
         static double SuperFruits = 0;
+        static int Coins = 0;
 
         internal override string Description()
         {
 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("INVENTORY");
+            Console.WriteLine();
+            Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("ITEMS");
             Console.ResetColor();
             Console.WriteLine("1. Fruits: " + Fruits);
-            Console.WriteLine("2. Super Fruits: " + SuperFruits);           
+            Console.WriteLine("2. Super Fruits: " + SuperFruits);
+            Console.WriteLine("3. Coins: " + Coins);
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("What would you like to use?");
             Console.WriteLine();
             Console.WriteLine("Come back to the [refugee]");
+            Console.ResetColor();
 
             return "";
         }
@@ -59,6 +65,14 @@ namespace FINAL_PROJECT_GV5.Places
                     }
                     break;
                 case "3":
+                    if(Coins > 0)
+                    {
+                        Betting();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You dont have enough Coins");
+                    }
                     break;
                 case "4":
                     break;
@@ -93,6 +107,92 @@ namespace FINAL_PROJECT_GV5.Places
         {
             SuperFruits += 1;
             return SuperFruits;
+        }
+        public double BettingCoins()
+        {
+            Coins += 1;
+            return Coins;
+        }
+
+        public int Betting()
+        {
+            Characters MainCharacter = Switch.MainCharacter;
+            int goldy;
+            int answer;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Currect gold: " + MainCharacter.gold);
+            Console.ResetColor();
+            while (true)
+            {
+                if(MainCharacter.gold <= 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You dont have money to bet");
+                    Console.ResetColor();
+                    Thread.Sleep(1500);
+                    Console.Clear();
+                    Game.Transition<Inventory>();
+                }
+                while (true)
+                {
+                    Console.WriteLine("Please insert how much gold you want to bet");
+                    string gain = Console.ReadLine();
+                    if (int.TryParse(gain, out goldy))
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please type a number");
+                        Console.ResetColor();
+                    }
+                }
+                Console.Write("You will bet: ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(goldy);
+                Console.Write(" GOLD");
+                Console.ResetColor();
+                Console.WriteLine("Press [1] to bet or [2] to come back to the Inventory");
+                Console.WriteLine(); 
+                answer = int.Parse(Console.ReadLine());
+                switch (answer)
+                {
+
+                    case 1:
+                        Random random = new Random();
+                        if (random.Next(2) == 1)
+                        {
+                            Console.ForegroundColor= ConsoleColor.Yellow;
+                            Console.WriteLine("YOU WON");
+                            Console.ResetColor();
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Game.Transition<Inventory>();
+                            Coins -= 1;
+                            return MainCharacter.gold = MainCharacter.gold + (goldy * 2);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("YOU LOSE");
+                            Console.ResetColor();
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                            Game.Transition<Inventory>();
+                            Coins -= 1;
+                            return MainCharacter.gold = MainCharacter.gold - goldy;
+                        }
+                    case 2:
+                        Console.Clear();
+                        Game.Transition<Inventory>();
+                        break;
+                    default: Console.WriteLine("Choose a valid option");
+                        break;
+                }
+            }
+           
         }
     }
 }
