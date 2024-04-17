4f944af717520ba75d00ba423f6f28ac3695d67e
Santiago <santysantos533@gmail.com>
2024-04-16 20:50:30 -0400

    Fixing weapon damages and connecting it to the dungeon
    The damage that each weapons does is now functioning and saved into a static variable.

--- Places/GunSmith.cs
+++ Places/GunSmith.cs
@@ -10,28 +10,30 @@ namespace OOP_PROJECT.Places
 {
     enum Weapons
     {
-        RustedMachete = 5,
-        PlasmaRepeater = 150,
-        ElectroKatana = 400,    
-        EPRK = 800,
+        RustedMachete = 1,
+        PlasmaRepeater = 100,
+        ElectroKatana = 200,    
+        EPRK = 400,
         InterstellarHypernovaCosmicDevastatorOfUniversalDestruction = 100000
     }
     internal class BlackSmith : Place
     {
-
+        public int WeaponDamage = 10;
         internal override string Description()
         {
             return @"Ah, welcome, welcome! What brings you to my forge today?
 In need of a repair, or perhaps something new? Whatever it is, you've come to the right place.
 Come, let's have a look and see what we can do for you
 
-1.Rusted machete - 5 gold
-2.Plasma Repeater - 1500 gold
-3.Electro-Katana - 3000 gold
-4.EPRK - 2 skulls 
-5. Interstellar Hypernova Cosmic Devastator of Universal Destruction - 100000
-6.Back to Refugee";
+1. Rusted- Machete (1 damage) - 5 gold
+2. Plasma-Repeater (100 damage) - 1500 gold
+3. Electro-Katana (200 damage) - 3000 gold
+4. EPRK (400 damage) - 1 skulls 
+5. Interstellar-Hypernova-Cosmic-Devastator-of-Universal-Destruction (100000 damage) - 100000
+6. Back to Refugee";
         }
+
+
         public bool GoldReturn(int price)
         {
             Characters MainCharacter = Main_Character_Description.Switch.MainCharacter;
@@ -50,19 +52,7 @@ Come, let's have a look and see what we can do for you
                 Console.WriteLine("Insuficient Gold");
                 return false;
             }
-        }
-        public bool HaveSkulls(bool OneSkull, bool TwoSkull)
-        {
-            if (OneSkull == true && TwoSkull == true)
-            {
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Are you sure you have the two skulls? ");
-                return false;
-            }
-        }
+        }       
         internal override void MovingAround(string choice2)
         {
             Characters MainCharacter = Main_Character_Description.Switch.MainCharacter;
@@ -72,40 +62,61 @@ Come, let's have a look and see what we can do for you
                 case "1":
                     
                     if (GoldReturn(5) == true)
-                    {
-                        
+                    {                       
                         MainCharacter.Weapon = Weapons.RustedMachete.ToString();
+                        WeaponDamage = (int)Weapons.RustedMachete;
                     }
-                    else if (GoldReturn(5) == false) { MainCharacter.Weapon = MainCharacter.Weapon; }
+                    else if (GoldReturn(5) == false) { Console.WriteLine("You dont have enough gold");
+                        MainCharacter.Weapon = MainCharacter.Weapon; }
                     break;
                 case "2":
                     if (GoldReturn(1500) == true)
-                    {
-                        GoldReturn(1500);
+                    {                       
                         MainCharacter.Weapon = Weapons.PlasmaRepeater.ToString();
+                        WeaponDamage = (int)Weapons.PlasmaRepeater;
                     }
-                    else if(GoldReturn(1500) == false) { MainCharacter.Weapon = MainCharacter.Weapon; }
+                    else if(GoldReturn(1500) == false) { Console.WriteLine("You dont have enough gold");
+                        MainCharacter.Weapon = MainCharacter.Weapon; }
                     break;
                 case "3":
                     if (GoldReturn(3000) == true)
-                    {
-                        GoldReturn(3000);
+                    {                       
                         MainCharacter.Weapon = Weapons.ElectroKatana.ToString();
+                        WeaponDamage = (int)Weapons.ElectroKatana;
                     }
-                    else if (GoldReturn(3000) == false) { MainCharacter.Weapon = MainCharacter.Weapon; }
+                    else if (GoldReturn(3000) == false) { Console.WriteLine("You dont have enough gold");
+                        MainCharacter.Weapon = MainCharacter.Weapon;
+                        goto case "6";
+                    }
                     break;
                 case "4":
-                   
+                    if(Game.Firstskull == true)
+                    {
+
+                        MainCharacter.Weapon = Weapons.EPRK.ToString();
+                        WeaponDamage = (int)Weapons.EPRK;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Are you sure you have two skulls?");
+                        MainCharacter.Weapon = MainCharacter.Weapon;
+                        goto case "6";
+                    }
                     break;
                 case "5":
                     if (GoldReturn(100000) == true)
                     {
-                        GoldReturn(100000);
                         MainCharacter.Weapon = Weapons.InterstellarHypernovaCosmicDevastatorOfUniversalDestruction.ToString();
+                        WeaponDamage = (int)Weapons.InterstellarHypernovaCosmicDevastatorOfUniversalDestruction;
                     }
-                    else if (GoldReturn(3000) == false) { MainCharacter.Weapon = MainCharacter.Weapon; }
+                    else if (GoldReturn(3000) == false) { Console.WriteLine("You dont have enough gold");
+
+                        MainCharacter.Weapon = MainCharacter.Weapon; }
+                        goto case "6";
                     break;
                 case "6":
+                    Console.Clear();
                     Game.Transition<Refugee>();
                     break;
                 default:
